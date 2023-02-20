7463191c04ea772ba1389cd5346cb865c7d4e926
frnc96 <frencis.balla@gmail.com>
2023-02-20 15:13:17 +0100

    init

---
+++ src/datasets.py
@@ -0,0 +1,125 @@
+# %%
+from typing import Literal, Tuple, Union
+import pandas as pd
+import glob
+import torch
+from torch.utils.data import Dataset
+import random
+
+
+class TensorDataset(Dataset):
+    DATAFILE_EXTENSION = ".pt"
+
+    def __init__(self, data_dirs: Union[str, list[str]], target: Literal["train", "test", "validation"]="train"):
+        self.target = target
+        if isinstance(data_dirs, str):
+            data_dirs = [data_dirs]
+        
+        self.data_dir = data_dirs
+
+        # the training files are taken from all the data directories
+        self.train_files = []
+        for data_dir in data_dirs:
+            self.train_files.extend(glob.glob(f"{data_dir}/train/*{TensorDataset.DATAFILE_EXTENSION}"))
+
+        # validation and testing is taken only from the last data directory
+        data_dir = data_dirs[-1] 
+        self.validation_files = glob.glob(f"{data_dir}/validation/*{TensorDataset.DATAFILE_EXTENSION}")
+        self.test_files = glob.glob(f"{data_dir}/test/*{TensorDataset.DATAFILE_EXTENSION}")
+        if self.train_files is None or self.validation_files is None or self.test_files is None:
+            return
+        self.train_files.sort()
+        self.validation_files.sort()
+        self.test_files.sort()
+
+    def __len__(self):
+        if self.target == "train":
+            return len(self.train_files)
+        if self.target == "test":
+            return len(self.test_files)
+        if self.target == "validation":
+            return len(self.validation_files)
+
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        if self.target == "train":
+            return torch.load(self.train_files[idx])
+        if self.target == "test":
+            return torch.load(self.test_files[idx])
+        if self.target == "validation":
+            return torch.load(self.validation_files[idx])
+
+    def get_sample(self, source: Literal["train", "test", "validation"] = "validation", random_sample=False) -> Tuple[torch.Tensor, torch.Tensor]:
+        if source == "train":
+            idx = 0 if not random_sample else random.randint(0, len(self.train_files) - 1)
+            return torch.load(self.train_files[idx])
+        if source == "test":
+            idx = 0 if not random_sample else random.randint(0, len(self.test_files) - 1)
+            return torch.load(self.test_files[idx])
+        if source == "validation":
+            idx = 0 if not random_sample else random.randint(0, len(self.validation_files) - 1)
+            return torch.load(self.validation_files[idx])
+        raise ValueError()
+    
+    def get_start_index(self, target: Literal["train", "test", "validation"]="test") -> int:
+        if target == "train":
+            return 0
+        if target == "validation":
+            return len(self.train_files) 
+        if target == "test":
+            return len(self.train_files) + len(self.validation_files)
+        raise ValueError()
+
+    def convert_to_millivolts(self, input):
+        return input
+
+    
+
+class PTB_Dataset(TensorDataset):
+    MAX_VALUE = 33  # 32.715999603271484
+    header = ["I", "II", "III", "aVF", "aVR", "aVL", "V1", "V2", "V3", "V4", "V5", "V6"]
+
+    def __init__(self, data_dirs: Union[str, list[str]], target: Literal["train", "test", "validation"] = 'train'):
+        super().__init__(data_dirs, target)
+
+    @staticmethod
+    def convert_input(input):
+        return input / PTB_Dataset.MAX_VALUE
+
+    @staticmethod
+    def convert_output(tensor_out):
+        return tensor_out * PTB_Dataset.MAX_VALUE
+
+    def max_value(self):
+        files = glob.glob(self.data_dir + '/*.csv')
+        max_value = 0
+        for file in files:
+            temp_df = pd.read_csv(file, index_col=0, header=0, names=PTB_Dataset.header)
+            temp_tensor_in = torch.tensor(temp_df.iloc[:, 0], dtype=torch.float32).unsqueeze(0)
+            temp_tensor_out = torch.tensor(temp_df.iloc[:, [1, 6, 7, 8, 9, 10, 11]].values, dtype=torch.float32)
+            max_in = torch.max(temp_tensor_in).item()
+            min_in = torch.min(temp_tensor_in).item()
+            max_out = torch.max(temp_tensor_out).item()
+            min_out = torch.min(temp_tensor_out).item()
+            max_value = max(max_value, max_in, max_out, abs(min_in), abs(min_out))
+        print(max_value)
+
+    def convert(self):
+        for index in range(0, len(glob.glob(self.data_dir + '/*.csv'))):
+            temp_df = pd.read_csv(f"{self.data_dir}/ecg{index}.csv", index_col=0, header=0, names=PTB_Dataset.header)
+            temp_tensor_in = torch.tensor(PTB_Dataset.convert_input(temp_df.iloc[:, 0]), dtype=torch.float32).unsqueeze(0)
+            temp_tensor_out = torch.tensor(PTB_Dataset.convert_input(temp_df.iloc[:, [1, 6, 7, 8, 9, 10, 11]].values), dtype=torch.float32).t()
+            temp_tensor_pair = (temp_tensor_in, temp_tensor_out)
+            torch.save(temp_tensor_pair, f'{self.data_dir}/ecg{str(index).zfill(5)}.pt')
+
+
+
+# %%
+# dataset_folder = 'PTB'
+# transformer = PTB_Dataset(dataset_folder)
+# transformer.convert()
+# %%
+# dataset_folder = 'PTB_pathologic'
+# transformer = PTB_Dataset(dataset_folder)
+# transformer.convert()
+# transformer.max_value()
+
---
+++ src/main.py
@@ -0,0 +1,89 @@
+#%%
+from network import Pulse2pulseGenerator
+from datasets import PTB_Dataset, TensorDataset
+
+import torch
+import numpy as np
+from pathlib import Path
+from typing import Tuple
+import random
+from datetime import datetime
+
+
+ACTION = 'train'               # train | test | generate_outputs
+
+
+############# CONFIGURATION ###############
+train_configuration = {
+    'DATASET_OPTION': ['PTB'],               # synthetic | PTB | PTB_pathologic | ['synthetic', 'PTB']
+    'NETWORK_OPTION': 'AE',                    # GAN | AE
+    'TRAINING_MODE': 'simple',         # simple | transfer
+    'LEARNING_RATE': 0.0001,
+    'MODEL_SIZE': 16,
+    'BATCH_SIZE': 32,
+    'EPOCHS': 200,
+    'DISCRIMINATOR_PATCH_SIZE': 1000
+}
+############################################
+
+
+def fix_seed():
+    # torch.use_deterministic_algorithms(True)
+    # import os
+    # os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8' # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+    random_seed = 123
+    random.seed(random_seed)
+    np.random.seed(random_seed)
+    torch.manual_seed(random_seed)
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False 
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if ACTION=='train':
+    fix_seed()
+
+
+def get_dataloader(target='train', batch_size = train_configuration['BATCH_SIZE'], shuffle = True) -> Tuple[TensorDataset, torch.utils.data.DataLoader]:
+    dataset = PTB_Dataset(data_dirs = train_configuration['DATASET_OPTION'], target = target)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size = min(batch_size, len(dataset)), shuffle = shuffle, drop_last = True)
+    return dataset, dataloader
+
+
+def train_ae(train_dataset: TensorDataset, train_dataloader, validation_dataset: TensorDataset, validation_dataloader):
+    generator = Pulse2pulseGenerator(model_size=train_configuration['MODEL_SIZE']).to(device)
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(generator.parameters(), lr=train_configuration['LEARNING_RATE'], betas=(0.5, 0.999))
+    train_loss_plot = []
+
+    for epoch in range(train_configuration['EPOCHS']):
+        generator.train()
+        train_loss_average = 0
+        for batch, (leadI, leadsII_VIII) in enumerate(train_dataloader, 0):
+            if batch % 20 == 0: print(f'Batch: {batch}, time {datetime.now()}')
+
+            leadI = leadI.to(device)
+            leadsII_VIII = leadsII_VIII.to(device)
+
+            output = generator(leadI)
+            train_criterion = criterion(train_dataset.convert_to_millivolts(train_dataset.convert_output(output)), 
+                                        train_dataset.convert_to_millivolts(train_dataset.convert_output(leadsII_VIII)))
+            train_loss_average += train_criterion.data.cpu()
+            optimizer.zero_grad()
+            train_criterion.backward()
+            optimizer.step()
+
+        train_loss_average /= len(train_dataloader)
+        train_loss_plot.append(train_loss_average)
+
+
+
+if ACTION == "train":
+    train_dataset, train_dataloader = get_dataloader(target='train')
+    validation_dataset, validation_dataloader = get_dataloader(target='validation', batch_size=1, shuffle=False)
+    train_ae(train_dataset, train_dataloader, validation_dataset, validation_dataloader)
+
+
+
+# %%
