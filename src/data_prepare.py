0a6a0ee901ad3ea0f9a5391aaf1a8d008515652f
frnc96 <frencis.balla@gmail.com>
2023-05-09 14:45:24 +0200

    ...

--- src/data_prepare.py
+++ src/data_prepare.py
@@ -6,7 +6,7 @@ import os
 # To prepare the data place the dataset .zip file
 # in the data directory and run this script
 
-raw_data_path = os.path.abspath('../data/PTB.zip')
+raw_data_path = os.path.abspath('PTB.zip')
 with zipfile.ZipFile(raw_data_path, 'r') as zip_ref:
     zip_ref.extractall()
 
